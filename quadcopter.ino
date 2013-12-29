f84b85fd89bbdaa78dd930748cb893656991faf2
Charlie Smurthwaite <charlie@atechmedia.com>
2013-12-29 17:44:57 +0000

    documentation and tidying

--- quadcopter.ino
+++ quadcopter.ino
@@ -8,31 +8,31 @@
 #include "Wire.h"
 
 // Settings
-#define POSITION_FEEDBACK 300.0     // This is the pitch/roll feedback amount
-#define POSITION_FEEDBACK_Z 1.0     // This is the yaw feedback amount
-#define GYRO_FEEDBACK  0.06         // Rotational velovity correction
-#define CONTROL_SENSITIVITY 0.6     // Pitch/roll control sensitivity
-#define CONTROL_SENSITIVITY_Z 1.0   // Yaw control sensitivity
-#define INTEGRATION_AMOUNT 0.002    // Pitch/roll integration amount
+#define POSITION_FEEDBACK 300.0       // This is the pitch/roll feedback amount
+#define POSITION_FEEDBACK_Z 1.5       // This is the yaw feedback amount
+#define GYRO_FEEDBACK  0.06           // Rotational velovity correction
+#define CONTROL_SENSITIVITY 0.6       // Pitch/roll control sensitivity
+#define CONTROL_SENSITIVITY_Z 1.0     // Yaw control sensitivity
+#define INTEGRATION_AMOUNT 0.002      // Pitch/roll integration amount
 
 // Global Variables and Objects
-double smoothed_control_x=0;        // Smoothed RC Input
-double smoothed_control_y=0;        // Smoothed RC Input
-double smoothed_control_t=0;        // Smoothed RC Input
-double smoothed_control_z=0;      // Smoothed RC Input
-int armed=0;                        // Motors enabled
+double smoothed_control_x=0;          // Smoothed RC Input
+double smoothed_control_y=0;          // Smoothed RC Input
+double smoothed_control_t=0;          // Smoothed RC Input
+double smoothed_control_z=0;          // Smoothed RC Input
 
-double pos_x, pos_y, pos_z;              // IMU input
-double gyro_x, gyro_y, gyro_z;           // Gyro Input
-double output_x, output_y, output_z;     // Stabilization Output
-double altitude_hold_correction;
-double altitude_hold_control;
-double integrated_x=0;
-double integrated_y=0;
-double integrated_z=0;
+int armed=0;                          // Motors enabled
+double pos_x, pos_y, pos_z;           // IMU input
+double gyro_x, gyro_y, gyro_z;        // Gyro Input
+double output_x, output_y, output_z;  // Stabilization Output
+double altitude_hold_correction;      // Altitude hold output
+double altitude_hold_control;         // Altitude hold amount selection
 
-MPU6050 mpu;             // Motion processor
-bool dmpReady = false;   // set true if DMP init was successful
+double integrated_x=0;                // Integration of roll
+double integrated_y=0;                // Integration of pitch
+
+MPU6050 mpu;                          // Motion processor
+bool dmpReady = false;                // set true if MPU initizlization was successful
 
 void loop()
 {
@@ -42,29 +42,34 @@ void loop()
   // Fetch data
   mpuGetXY();
   
-  // Process data
+  // Process RC data
   process_rc_data();
+  
+  // Calculate proportial responses with gyro correction
   double x, y, z;
   x = smoothed_control_x - pos_x * POSITION_FEEDBACK - gyro_x * GYRO_FEEDBACK;
   y = smoothed_control_y - pos_y * POSITION_FEEDBACK - gyro_y * GYRO_FEEDBACK;
   z = smoothed_control_z - pos_z;
-  if (z >  180) z -= 360;
-  if (z < -180) z += 360;
+  if (z >  180) z -= 360; // Try to find the most efficient path to correct yaw
+  if (z < -180) z += 360; // Try to find the most efficient path to correct yaw
   
-  output_x = x + integrated_x;
-  output_y = y + integrated_y;
-  output_z = z;
-  
+  // Integrate pitch and roll to counter any permanent imbalance
   integrated_x += x * INTEGRATION_AMOUNT;
   integrated_y += y * INTEGRATION_AMOUNT;
-  if(smoothed_control_t < 100) {
+  
+  // Reset integrals if throttle is zero
+  if(smoothed_control_t < 50) {
     integrated_x = 0;
     integrated_y = 0;
   }
   
-  altitude_hold_correction = (sq(pos_x) + sq(pos_y)) * altitude_hold_control;
-  Serial.print(altitude_hold_correction),
-  Serial.print("\n");
+  // Apply the integrals
+  output_x = x + integrated_x;
+  output_y = y + integrated_y;
+  output_z = z;
+  
+  // Calculate throttle correction based on pitch/roll and control input
+  altitude_hold_correction = (pos_x + pos_y) * altitude_hold_control;
   
   // Push data to motors
   set_velocities();
