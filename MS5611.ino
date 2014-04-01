72d18917e2496c67f50c0db7bd1784adafcd640c
Charlie Smurthwaite <charlie@atechmedia.com>
2014-04-01 23:00:45 +0100

    a totally new take on altitude hold, safer yaw correction, i currently have concerns about pitch/roll integration

--- MS5611.ino
+++ MS5611.ino
@@ -1,11 +1,11 @@
 #include "RunningMedian.h"
-RunningMedian samples = RunningMedian(6);
+RunningMedian samples = RunningMedian(10);
 
 void msGetPressure() {
   float t, p;
   barometer.readValues(&p, &t);
   samples.add(p*1000);
-  pressure = samples.getAverage(4);
+  pressure = samples.getAverage(6);
 }
 
 void msSetup() {
